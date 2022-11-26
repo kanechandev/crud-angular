143f95515e27ade508b45ea033ff6336c4b2be9f
Kane Chan <kanes.chan@gmail.com>
2022-11-26 10:52:24 -0300

    Criado Material Table - Listar Cursos.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { Course } from './../model/course';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
+
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'front-end'}
+  ];
+  displayedColumns = ['name', 'category'];
 
 }
