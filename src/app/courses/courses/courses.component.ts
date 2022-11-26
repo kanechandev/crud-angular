f395985fa5fc4c59b5ea7aaa24b5d683bffbdbdd
Kane Chan <kanes.chan@gmail.com>
2022-11-26 17:18:19 -0300

    Trabalhado Spinner.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -10,14 +10,14 @@ import { Observable } from 'rxjs';
 })
 export class CoursesComponent {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
  //coursesService: CoursesService;
 
   constructor(private coursesService: CoursesService){
     //this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 
 }
