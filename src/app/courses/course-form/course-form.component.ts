713ecb279cfc064c0c36cbf70835f33be38a746c
Kane Chan <kanes.chan@gmail.com>
2023-01-22 21:26:43 -0300

    Pré envio formulário e tratamento de erros.

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -10,7 +13,9 @@ export class CourseFormComponent{
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder){
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar){
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -18,10 +23,17 @@ export class CourseFormComponent{
   }
 
   onSubmit(){
-
+    this.service.save(this.form.value).subscribe({
+      next: (data) => console.log(data),
+      error: () => this.onError()
+    });
   }
 
   onCancel(){
 
+  }
+
+  private onError(){
+    this.snackBar.open('Erro ao salvar curso', '', { duration: 3000});
   }
 }
